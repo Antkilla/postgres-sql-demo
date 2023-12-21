0f2acf46cacf555c0c663e71287644a150f12b54
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-12-20 17:07:58 -0800

    Update postgres.sql

--- server/postgres.sql
+++ server/postgres.sql
@@ -1,3 +1,9 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100)
+);
+
 CREATE TABLE profiles (
   user-id INT PRIMARY KEY,
   birthday DATE,
@@ -5,3 +11,8 @@ CREATE TABLE profiles (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+INSERT INTO users (name, email) VALUES
+  ('Marie', 'marie@gmail.com'),
+  ('Jessy', 'jessy2@gmail.com'),
+  ('Linus', 'linus@hotmail.com'),
+  ('Molly', 'molly@hotmail.com');
